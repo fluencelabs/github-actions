3f507f21fb4b03c0b1891d74f11ef730e08ff956
Anatoly Laskaris <github_me@nahsi.dev>
2022-11-16 10:16:52 +0200

    Fix

--- cargo-publish-snapshot/cargo-set-registry.py
+++ cargo-publish-snapshot/cargo-set-registry.py
@@ -8,6 +8,8 @@ try:
     for dependency in manifest["dependencies"]:
         if dependency in sys.argv[1:]:
             manifest["dependencies"][dependency]["registry"] = "fluence"
+except:
+    pass
 
 with open("Cargo.toml", "w") as f:
     f.write(toml.dumps(manifest))
