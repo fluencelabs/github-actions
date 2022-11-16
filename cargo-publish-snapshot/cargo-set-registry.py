4e32ae2bf48e4216796f95c455c621f509323ad8
Anatoly Laskaris <github_me@nahsi.dev>
2022-11-16 10:14:57 +0200

    Update

--- cargo-publish-snapshot/cargo-set-registry.py
+++ cargo-publish-snapshot/cargo-set-registry.py
@@ -4,9 +4,10 @@ import toml
 import sys
 
 manifest = toml.load("Cargo.toml")
-for dependency in manifest["dependencies"]:
-    if dependency in sys.argv[1:]:
-        manifest["dependencies"][dependency]["registry"] = "fluence"
+try:
+    for dependency in manifest["dependencies"]:
+        if dependency in sys.argv[1:]:
+            manifest["dependencies"][dependency]["registry"] = "fluence"
 
 with open("Cargo.toml", "w") as f:
     f.write(toml.dumps(manifest))
