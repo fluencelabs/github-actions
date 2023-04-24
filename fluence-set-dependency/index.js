2d3ffdf4507711d2dca57cadca62bb8d65cb9346
Anatoly Laskaris <github_me@nahsi.dev>
2023-04-24 18:30:00 +0300

    Debug

--- fluence-set-dependency/index.js
+++ fluence-set-dependency/index.js
@@ -5,11 +5,20 @@ const path = require("path");
 
 // Read the project path from the input variable
 const projectPath = core.getInput("path");
+
+// Get the GitHub workspace directory
+const workspaceDirectory = process.env.GITHUB_WORKSPACE;
+
+// Construct the absolute path to the project directory
+const absoluteProjectPath = path.join(workspaceDirectory, projectPath);
+
+// Log the current working directory, project path, and absolute project path
 console.log("Current working directory:", process.cwd());
 console.log("Project path:", projectPath);
+console.log("Absolute project path:", absoluteProjectPath);
 
 // Construct the path to the fluence.yaml file
-const fluenceFilePath = path.join(projectPath, "fluence.yaml");
+const fluenceFilePath = path.join(absoluteProjectPath, "fluence.yaml");
 
 // Read and parse fluence.yaml
 const fluenceYaml = fs.readFileSync(fluenceFilePath, "utf8");
