d3cdb36e7c958295897ea73817a7838414cbcd35
Anatoly Laskaris <github_me@nahsi.dev>
2023-05-16 14:40:03 +0300

    Update

--- fluence-healthcheck/src/index.js
+++ fluence-healthcheck/src/index.js
@@ -5,60 +5,62 @@ import * as network from "@fluencelabs/fluence-network-environment";
 import { checkPeer } from "./_aqua/main.js";
 
 async function main() {
-  console.log(process.env);
   const env = core.getInput("env");
   const timeout = parseInt(core.getInput("timeout"));
-  let peers, relay;
-  console.log(process.env.GITHUB_CONTEXT);
-  console.log(env);
+  const peers = getPeersByEnv(env);
 
   const errorMessages = [];
+  let connected = false;
 
-  switch (env) {
-    case "stage":
-      peers = network.stage;
-      relay = network.randomStage();
+  for (let retryCount = 0; retryCount < 3; retryCount++) {
+    const relay = getRandomRelay(peers);
+
+    try {
+      await Fluence.connect(relay.multiaddr);
+      connected = true;
       break;
-    case "testnet":
-      peers = network.testNet;
-      relay = network.randomTestNet();
-      break;
-    case "kras":
-      peers = network.kras;
-      relay = network.randomKras();
-      break;
-    default:
-      console.error('Invalid "env" input. Use "stage", "testnet", or "kras".');
-      process.exit(1);
+    } catch (e) {
+      console.error(`Error connecting to ${relay.multiaddr}:\n ${e}`);
+      errorMessages.push(`Error connecting to ${relay.multiaddr}:\n ${e}`);
+    }
   }
 
-  try {
-    await Fluence.connect(relay.multiaddr);
+  if (connected) {
+    try {
+      const data = generatePeerData(peers);
+      const results = [];
 
-    const data = peers.map(({ peerId, multiaddr }, index) => ({
-      peer: peerId,
-      multiaddr: multiaddr,
-      validators: peers.filter((_, idx) => idx !== index).map((n) => n.peerId),
-    }));
+      for (const setup of data) {
+        try {
+          console.log(`Checking peer: ${setup.multiaddr}`);
+          const result = await checkPeer(
+            setup.peer,
+            setup.validators,
+            timeout,
+            {
+              ttl: timeout,
+            },
+          );
 
-    for (const setup of data) {
-      try {
-        console.log(`Checking peer: ${setup.multiaddr}`);
-        const result = await checkPeer(setup.peer, setup.validators, timeout, {
-          ttl: timeout,
-        });
-        console.log(result);
-      } catch (e) {
-        const errorMessage = JSON.stringify(e, null, 2);
-        console.error(errorMessage);
-        errorMessages.push(`${setup.multiaddr} failed:\n ${errorMessage}`);
+          results.push(result);
+        } catch (e) {
+          const errorMessage = JSON.stringify(e, null, 2);
+          console.error(errorMessage);
+          errorMessages.push(`${setup.multiaddr} failed:\n ${errorMessage}`);
+        }
       }
+
+      if (results.every(isTargetReachable)) {
+        console.log(results);
+      } else {
+        throw new Error("Target is not reachable or other error occurred.");
+      }
+    } catch (e) {
+      console.error(e);
+      errorMessages.push(`Error during peer checks:\n ${e}`);
+    } finally {
+      await Fluence.disconnect();
     }
-  } catch (e) {
-    console.error(`Error connecting to ${relay.multiaddr}:\n ${e}`);
-    errorMessages.push(`Error connecting to ${relay.multiaddr}:\n ${e}`);
-  } finally {
-    await Fluence.disconnect();
   }
 
   if (errorMessages.length > 0) {
@@ -66,6 +68,37 @@ async function main() {
     console.log(errorMessages.join("\n"));
     process.exit(1);
   }
+}
+
+function getPeersByEnv(env) {
+  switch (env) {
+    case "stage":
+      return network.stage;
+    case "testnet":
+      return network.testNet;
+    case "kras":
+      return network.kras;
+    default:
+      console.error('Invalid "env" input. Use "stage", "testnet", or "kras".');
+      process.exit(1);
+  }
+}
+
+function getRandomRelay(peers) {
+  const randomIndex = Math.floor(Math.random() * peers.length);
+  return peers[randomIndex];
+}
+
+function generatePeerData(peers) {
+  return peers.map(({ peerId, multiaddr }, index) => ({
+    peer: peerId,
+    multiaddr: multiaddr,
+    validators: peers.filter((_, idx) => idx !== index).map((n) => n.peerId),
+  }));
+}
+
+function isTargetReachable(result) {
+  return result.includes("status TARGET REACHABLE");
 }
 
 main().catch((error) => {
