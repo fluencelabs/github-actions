04934707ebfed3eb1ba231a3ae67440d54b2cfb8
Anatoly Laskaris <github_me@nahsi.dev>
2023-06-27 15:22:36 +0300

    Update

--- fluence-healthcheck/README.md
+++ fluence-healthcheck/README.md
@@ -1,8 +1,1 @@
 # How to use
-
-```yaml
-- uses: fluencelabs/github-actions/fluence-healthcheck@main
-  with:
-    env: stage # required
-    timeout: 15000 # optional. Defaults to 10000 ms
-```
--- fluence-healthcheck/src/index.js
+++ fluence-healthcheck/src/index.js
@@ -1,112 +1,110 @@
 import "@fluencelabs/js-client.node";
-import * as core from "@actions/core";
 import { Fluence } from "@fluencelabs/js-client.api";
 import * as network from "@fluencelabs/fluence-network-environment";
 import { checkPeer } from "./_aqua/main.js";
+import * as core from "@actions/core";
+import chalk from "chalk";
+
+const envs = {
+  stage: network.stage,
+  testnet: network.testNet,
+  kras: network.kras,
+};
+
+const getEnv = () => core.getInput("env");
+const getTimeout = () => parseInt(core.getInput("timeout"));
 
 async function main() {
   try {
-    const env = core.getInput("env");
-    const timeout = parseInt(core.getInput("timeout"));
-    const peers = getPeersByEnv(env);
+    const env = getEnv();
+    const timeout = getTimeout();
+    const peerList = envs[env];
 
-    await attemptToConnect(peers);
-    const data = generatePeerData(peers);
+    if (!peerList) {
+      core.setFailed('Invalid env. Use "stage", "testnet", or "kras".');
+    }
 
-    let errorMessages = [];
-    for (let setup of data) {
-      const checkResult = await performCheck(setup, timeout, errorMessages);
-      if (!isTargetReachable(checkResult)) {
-        errorMessages.push(checkResult);
+    await establishConnection(peerList);
+    const peerData = constructPeerData(peerList);
+
+    let failureMessages = [];
+    for (const peerSetup of peerData) {
+      const checkResult = await validateConnectivity(peerSetup, timeout);
+      if (!isPeerReachable(checkResult)) {
+        failureMessages.push(checkResult);
       }
     }
 
     await Fluence.disconnect();
+
+    if (failureMessages.length) {
+      core.setFailed("Some target checks were unsuccessful.");
+      core.setOutput("error_log", failureMessages.flat().join("\n"));
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function attemptToConnect(peers) {
+async function establishConnection(peerList) {
   for (let retryCount = 0; retryCount < 3; retryCount++) {
-    const relay = getRandomRelay(peers);
-
+    const relay = selectRandomRelay(peerList);
     try {
       await Fluence.connect(relay.multiaddr);
       return;
-    } catch (e) {
-      console.error(`Error connecting to ${relay.multiaddr}:\n ${e}`);
+    } catch (error) {
+      core.info(`Error connecting to ${relay.multiaddr}:\n ${error}`);
     }
   }
-  throw new Error("Could not connect to relay.");
+  throw new Error("Could not connect to relay after 3 tries.");
 }
 
-async function performCheck(setup, timeout, errorMessages) {
-    console.log(`Checking target: ${setup.multiaddr}`);
-    let validators = setup.validators;
-
-    let checkResult = null;
-    for (let validator of validators) {
-        try {
-            const result = await checkPeer(setup.peer, validator.peerId, timeout, {
-                ttl: timeout,
-            });
-
-            if (isTargetReachable(result)) {
-                console.log(`Target reachable from ${validator.multiaddr}`);
-            } else {
-                console.log(`Target not reachable from ${validator.multiaddr}`);
-            }
-
-            checkResult = { ...result, peer: setup.multiaddr };
-        } catch (e) {
-            const errorMessage = `Target check from ${validator.multiaddr} failed with error:\n ${
-              JSON.stringify(e, null, 2)
-            }`;
-            console.error(errorMessage);
-            errorMessages.push(errorMessage);
-        }
+async function validateConnectivity(peerSetup, timeout) {
+  core.info(`Checking target: ${peerSetup.multiaddr}`);
+  let errorList = [];
+  for (const validator of peerSetup.validators) {
+    try {
+      const result = await checkPeer(
+        peerSetup.peer,
+        validator.peerId,
+        timeout,
+        { ttl: timeout },
+      );
+      const isReachable = isPeerReachable(result);
+      core.info(
+        isReachable
+          ? chalk.green(`Target reachable from ${validator.multiaddr}`)
+          : chalk.red(`Target not reachable from ${validator.multiaddr}`),
+      );
+    } catch (error) {
+      const errorMessage =
+        `Target ${peerSetup.multiaddr} check from ${validator.multiaddr} failed with error:\n ${
+          JSON.stringify(error, null, 2)
+        }`;
+      core.info(chalk.red(errorMessage));
+      errorList.push(errorMessage);
     }
-
-    return checkResult;
-}
-
-function getPeersByEnv(env) {
-  switch (env) {
-    case "stage":
-      return network.stage;
-    case "testnet":
-      return network.testNet;
-    case "kras":
-      return network.kras;
-    default:
-      console.error('Invalid "env" input. Use "stage", "testnet", or "kras".');
-      process.exit(1);
   }
+  return errorList;
 }
 
-function getRandomRelay(peers) {
-  const randomIndex = Math.floor(Math.random() * peers.length);
-  return peers[randomIndex];
-}
+const selectRandomRelay = (peerList) =>
+  peerList[Math.floor(Math.random() * peerList.length)];
 
-function generatePeerData(peers) {
-  return peers.map(({ peerId, multiaddr }, index) => ({
+function constructPeerData(peerList) {
+  return peerList.map(({ peerId, multiaddr }, index) => ({
     peer: peerId,
     multiaddr: multiaddr,
-    validators: peers
+    validators: peerList
       .filter((_, idx) => idx !== index)
-      .map((n) => ({ peerId: n.peerId, multiaddr: n.multiaddr })),
+      .map(({ peerId, multiaddr }) => ({ peerId, multiaddr })),
   }));
 }
 
-function isTargetReachable(result) {
-  for (const key in result) {
-    if (result[key].includes("TARGET REACHABLE")) {
-      return true;
-    }
-  }
-  return false;
+function isPeerReachable(checkResult) {
+  return Object.values(checkResult).some((value) =>
+    value.includes("TARGET REACHABLE")
+  );
 }
 
 main();
